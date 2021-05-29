e8922048fa5fa9b2812bf24f3a422778cfe7bea8
Fujiwara <r9.20important@icloud.com>
2021-05-29 18:04:15 +0900

    CRUD API追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -23,11 +23,15 @@ class TaskController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
+        $task = Task::create($request->all());
+
+        return $task
+            ? response()->json($task,201)
+            : response()->json([], 500);
     }
 
     /**
@@ -47,21 +51,27 @@ class TaskController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, Task $task)
     {
-        //
+        $task->title = $request->title;
+
+        return $task->update()
+            ? response()->json($task)
+            : response()->json([], 500);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Task $task)
     {
-        //
+        return $task->delete()
+            ? response()->json($task)
+            : response()->json([], 500);
     }
 }
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -21,4 +21,35 @@ class ExampleTest extends TestCase
         $response->assertStatus(200)
                  ->assertJsonCount($tasks->count());
     }
+
+    public function test_regist()
+    {
+        $data = [
+            'title' => 'テスト投稿'
+        ];
+        $response = $this->postJson('api/tasks', $data);
+        $response->assertCreated()
+                 ->assertJsonFragment($data);
+    }
+
+    public function test_update()
+    {
+        $task = Task::factory()->create();
+        $task->title = '書き換え';
+        $response = $this->patchJson("api/tasks/{$task->id}", $task->toArray());
+        $response->assertOk()
+                 ->assertJsonFragment($task->toArray());
+    }
+
+    public function test_delete()
+    {
+        $tasks = Task::factory()->count(10)->create();
+        
+        $response = $this->deleteJson("api/tasks/1");
+        $response->assertOk();
+        $response = $this->getJson("api/tasks");
+        $response->assertJsonCount($tasks->count() -1);
+    }
 }
+
+
